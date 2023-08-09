0ae14ddc006e459763fa4f2bcbcfb15c22364fbe
Milena Trajanoska <68651081+MilenaTrajanoska@users.noreply.github.com>
2023-08-09 21:53:53 +0200

    Update data path in example

--- examples/semantic_mapper_columns_basic.py
+++ examples/semantic_mapper_columns_basic.py
@@ -15,7 +15,7 @@ logging.getLogger().addHandler(logging.FileHandler(filename=logfile))
 
 if __name__ == '__main__':
 
-    data = pd.read_csv('C:/Users/mimi_/Desktop/Purchases.csv')
+    data = pd.read_csv('./data/fashion_products.csv')
 
     mapper = SemanticMapper(
         ontology_source_dir='../schemas',
@@ -25,9 +25,9 @@ if __name__ == '__main__':
     )
     res = mapper.map(
         columns=data.head(10).to_dict(),
-        description="The table contains data about product purchases. It includes customer identifiers, shipping information, dates of shipment, documents related to the shipments and price information.",
+        description="The table contains product fashion products information.",
         check_answers_llm='gpt-4'
     )
 
-    with open('data/mapping_result_fashion.json', 'w') as f:
+    with open('./data/mapping_result_fashion.json', 'w') as f:
         f.write(json.dumps(res))
